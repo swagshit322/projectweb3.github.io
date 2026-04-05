d69e8a025cb4a122393671f6d7e4c2108f3c41ca
malik <m4lstrim@yandex.ru>
2026-04-05 11:27:51 +0300

    test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 // test_connection.php
 try {
     $pdo = new PDO(
-        "mysql:host=localhost;dbname=u82671;charset=utf8mb4",
+        "mysql:host=localhost;dbname=u82665;charset=utf8mb4",
         "u82665",
         "3079533"
     );
